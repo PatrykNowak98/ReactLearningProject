2abd0671c087d9dc41997577bf0059f39851d02c
Patryk Nowak <patryk.nowak@taskscape.com>
2025-10-28 16:58:27 +0100

    ListGroup Implementation

---
+++ ReactApp/src/components/ListGroup.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+interface listGroupProps {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: listGroupProps) {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  return (
+    <>
+      <h1>{heading}</h1>
+      {items.length === 0 && <p>No item found</p>}{" "}
+      {/* If true then render <p>, otherwise the whole expression is false */}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
