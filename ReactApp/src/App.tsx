aa09607b7cea024d6f945ff98ef61600f82151a2
Patryk Nowak <patryk.nowak@taskscape.com>
2025-12-08 17:23:36 +0100

    Showing a loading indicator

--- ReactApp/src/App.tsx
+++ ReactApp/src/App.tsx
@@ -11,19 +11,33 @@ interface User {
 function App() {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
 
+    setLoading(true);
     axios
       .get<User[]>("https://jsonplaceholder.typicode.com/users", {
         signal: controller.signal,
       })
-      .then((res) => setUsers(res.data))
+      .then((res) => {
+        setUsers(res.data);
+        setLoading(false);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
-        setError(err.message);
+        {
+          {
+            setError(err.message);
+            setLoading(false);
+          }
+        }
       });
+    // Prefferable way but does not appear in strict mode, if needed remove the setup of loading for then field and catch field
+    // .finally(() => {
+    //   setLoading(false);
+    // });
 
     return () => controller.abort();
   }, []);
@@ -31,6 +45,7 @@ function App() {
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
+      {isLoading && <div className="spinner-border"></div>}
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
